f8149a7163228d92ff1d636727f788cd61cbf1e7
sofieqvist <36158710+sofieqvist@users.noreply.github.com>
2018-02-09 13:04:40 +0100

    put inline js in js file

--- js/javascript.js
+++ js/javascript.js
@@ -4,3 +4,20 @@ var app6 = new Vue({
     message: ''
   }
 })
+function myFunction() {
+if (document.forms["colors"].color.value == "blue") {
+window.location.href = "results.html"; return false;
+}
+if (document.forms["colors"].color.value == "orange") {
+window.location.href = ""; return false;
+}
+if (document.forms["colors"].color.value == "black") {
+window.location.href = "results.html"; return false;
+}
+if (document.forms["colors"].color.value == "beige") {
+window.location.href = ""; return false;
+}
+if (document.forms["colors"].color.value == "green") {
+window.location.href = ""; return false;
+}
+}
