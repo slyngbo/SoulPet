9cbec4fb0889ed44b22727a628f009078d6df6e9
sofieqvist <36158710+sofieqvist@users.noreply.github.com>
2018-02-09 13:25:06 +0100

    alert functions for all questions

--- js/javascript.js
+++ js/javascript.js
@@ -21,6 +21,18 @@ window.location.href = "resultstwo.html"; return false;
 if (document.forms["colors"].color.value == "green") {
 window.location.href = ""; return false;
 }
+if (document.forms["gender"].gender.value == "") {
+  alert("You'll have to choose your gender"); return false;
+}
+if (document.forms["relationships"].relationshipstatus.value == "") {
+  alert("You'll have to choose your relationship status"); return false;
+}
+if (document.forms["heights"].height.value == "") {
+  alert("You'll have to choose if you're taller than 170cm"); return false;
+}
+if (document.forms["rightleft"].handed.value == "") {
+  alert("You'll have to choose if you're right or left handed"); return false;
+}
 if (document.forms["colors"].color.value == "") {
   alert("You'll have to choose your favorite color"); return false;
 }
